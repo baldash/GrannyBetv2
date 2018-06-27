14dcde99c28f849099a7645501f628945fa8c7dd
baldas_h <hugo.baldassin@epitech.eu>
2018-06-27 16:53:58 +0200

    Initial commit

---
+++ Source/GrannyBetv2/GrannyBetv2GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrannyBetv2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRANNYBETV2_API AGrannyBetv2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
